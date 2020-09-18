0cee324096d0e92fb064865f4fb741743bf82283
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2020-09-19 01:53:12 +0300

    Fix problems with tests
      In TEST(TreeBuilderKdTreeQueries, Random3dPerformance) the
      correctness of the result was checked by comparing the
      addresses of two poiters. This does not work anymore.
      This was fixed by comparing the contents of the results.
    
      Why this was not noticed earlier is because the test
      generates so small triangles that most of the rays
      do not intersect any triangles (0 on clang++ and 2 on g++)
      and the code had only been compiled with clang++.
    
      This new issue was addressed by renaming Random3dPerformance
      to Random3dSmallTrianglePerformance and creating a new test
      TEST(TreeBuilderKdTreeQueries, Random3DLargeTrianglePerformance)
      with larger triangles and more intersections.

--- src/kd_tree_test.cpp
+++ src/kd_tree_test.cpp
@@ -14,6 +14,12 @@ namespace {
 MATCHER_P(VecEq, v, "should equal " + PrintToString(v)) {
   return (v - arg).norm() < EPS;
 }
+MATCHER_P(TriangleEq, v, "should equal " + PrintToString(v)) {
+  if ((v.p0 - arg.p0).norm() > EPS) return 0;
+  if ((v.p1 - arg.p1).norm() > EPS) return 0;
+  if ((v.p2 - arg.p2).norm() > EPS) return 0;
+  return 1;
+}
 MATCHER_P(TriangleVecEq, v, "should equal " + PrintToString(v)) {
   if (v.size() != arg.size()) return 0;
   for (size_t i = 0; i < v.size(); ++i) {
@@ -112,7 +118,6 @@ TEST(TreeBuilderKdTreeQueries, Random3d) {
       TrianglePoint tp = t.getClosestRayIntersection(r);
       TrianglePoint correct = firstRayTriangleIntersection(scene, r);
       ++n_rays;
-      if (tp.triangle == correct.triangle) ++n_same_answer;
       EXPECT_THAT(tp.bary_coords, VecEq(correct.bary_coords));
     }
   }
@@ -120,12 +125,13 @@ TEST(TreeBuilderKdTreeQueries, Random3d) {
 // tests the TreeBuilder by checking that the resulting kd tree
 // has good enough performance
 // (used as a proxy for a correctly built kd tree)
-TEST(TreeBuilderKdTreeQueries, Random3dPerformance) {
+TEST(TreeBuilderKdTreeQueries, Random3dSmallTrianglePerformance) {
   std::mt19937 mt(1337);
   std::chrono::duration<double, std::milli> kd_tree_time(0.0);
   std::chrono::duration<double, std::milli> naive_time(0.0);
   int n_rays = 0;
   int n_same_answer = 0;
+  int n_intersections = 0;
   for (int i = 0; i < 5; ++i) {
     std::vector<Triangle> scene =
         test::randomTriangleVector(0.0, 100.0, 0.05, 100000, mt);
@@ -142,14 +148,60 @@ TEST(TreeBuilderKdTreeQueries, Random3dPerformance) {
       naive_time += t2 - t1;
       ++n_rays;
       if (tp.triangle == correct.triangle) ++n_same_answer;
-      ASSERT_EQ(tp.triangle, correct.triangle);
-      EXPECT_THAT(tp.bary_coords, VecEq(correct.bary_coords));
+      // no intersection
+      if (tp.triangle == nullptr) {
+        ASSERT_EQ(tp.triangle, correct.triangle);
+      } else {
+        ++n_intersections;
+        ASSERT_THAT(*tp.triangle, TriangleEq(*correct.triangle));
+        EXPECT_THAT(tp.bary_coords, VecEq(correct.bary_coords));
+      }
     }
   }
   EXPECT_LE(30 * kd_tree_time, naive_time);
   std::cerr << "kd_tree_time: " << kd_tree_time.count() << std::endl;
   std::cerr << "naive_time: " << naive_time.count() << std::endl;
   std::cerr << "total rays: " << n_rays << std::endl;
+  std::cerr << "intersections: " << n_intersections << std::endl;
+}
+TEST(TreeBuilderKdTreeQueries, Random3dLargeTrianglePerformance) {
+  std::mt19937 mt(1337);
+  std::chrono::duration<double, std::milli> kd_tree_time(0.0);
+  std::chrono::duration<double, std::milli> naive_time(0.0);
+  int n_rays = 0;
+  int n_same_answer = 0;
+  int n_intersections = 0;
+  for (int i = 0; i < 5; ++i) {
+    std::vector<Triangle> scene =
+        test::randomTriangleVector(0.0, 100.0, 3, 20000, mt);
+    Tree t = buildKdTree(scene, 1.0, 40.0);
+    std::cerr << "tree " << i << " done " << std::endl;
+    for (int i = 0; i < 100; ++i) {
+      Ray r(test::randomVec3(0.0, 1.0, mt), test::randomVec3(0.0, 1.0, mt));
+      auto t0 = std::chrono::high_resolution_clock::now();
+      TrianglePoint tp = t.getClosestRayIntersection(r);
+      auto t1 = std::chrono::high_resolution_clock::now();
+      TrianglePoint correct = firstRayTriangleIntersection(scene, r);
+      auto t2 = std::chrono::high_resolution_clock::now();
+      kd_tree_time += t1 - t0;
+      naive_time += t2 - t1;
+      ++n_rays;
+      if (tp.triangle == correct.triangle) ++n_same_answer;
+      // no intersection
+      if (tp.triangle == nullptr) {
+        ASSERT_EQ(tp.triangle, correct.triangle);
+      } else {
+        ++n_intersections;
+        ASSERT_THAT(*tp.triangle, TriangleEq(*correct.triangle));
+        EXPECT_THAT(tp.bary_coords, VecEq(correct.bary_coords));
+      }
+    }
+  }
+  EXPECT_LE(30 * kd_tree_time, naive_time);
+  std::cerr << "kd_tree_time: " << kd_tree_time.count() << std::endl;
+  std::cerr << "naive_time: " << naive_time.count() << std::endl;
+  std::cerr << "total rays: " << n_rays << std::endl;
+  std::cerr << "intersections: " << n_intersections << std::endl;
 }
 
 }  // namespace
