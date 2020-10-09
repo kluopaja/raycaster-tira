ee79bdca625eca11faac12b8337a9232f028bbb8
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2020-10-09 14:41:10 +0300

    Add functionalities to Image class
      Some rendered images have very bright spots (for example caused by the
      spotlights reflecting from a mirror). Similarly bright pixels may be
      caused by sampling noise or by bugs in the code. If scaleMaxTo is used
      to transform these images, the result will mostly be black.
    
      To alleviate these problems two member functions were implemented:
        truncateToFraction will clip away some of the largest color values.
    
        toLog2 will transform the color values to logarithms of color values.
    
      At the same time, the Image related functions were moved
      to image.h and image.cpp files from raycaster.h and raycaster.cpp

---
+++ src/image.h
@@ -0,0 +1,47 @@
+#ifndef RAYCASTER_IMAGE_H
+#define RAYCASTER_IMAGE_H
+#include <cmath>
+#include <string>
+#include <vector>
+
+#include "geometry.h"
+class Image {
+ public:
+  Image(int x_resolution, int y_resolution);
+  void setColor(int x_pos, int y_pos, const Vec3& color);
+  // scales values to [0, 1]
+  void scaleMaxTo(double new_max);
+  // applies value = log_2((value + c)/c)
+  // for debugging.
+  void toLog2(double c);
+  // Let f be smallest value such that c of all pixel values are at most f
+  // then applies value = max(value, f)
+  void truncateToFraction(double c);
+  // saves image as PPM with sRGB encoded values
+  bool savePPM(const std::string& file);
+
+ private:
+  double linearToSrgb(double val);
+  double maxColorValue();
+  int bufferPos(int x, int y);
+  char floatToByte(double val);
+  int x_resolution;
+  int y_resolution;
+  std::vector<Vec3> buffer;
+};
+// can be used to check if the used ppm viewer displays the image correctly
+// the whole image should look gray when looked at
+// display native resolution
+// (if separate black and white pixels are seen on the left side, then
+// the monitor resolution is too small or the monitor is being looked
+// from a too short distance)
+Image generateGammaTestImage(int x_size, int y_size);
+inline void Image::setColor(int x, int y, const Vec3& color) {
+  assert(x >= 0 && x < x_resolution);
+  assert(y >= 0 && y < y_resolution);
+  assert(color[0] > -EPS && color[1] > -EPS && color[2] > -EPS);
+  buffer[(size_t)bufferPos(x, y)] = color;
+}
+inline int Image::bufferPos(int x, int y) { return y * x_resolution + x; }
+inline char Image::floatToByte(double val) { return std::round(val * 255); }
+#endif
--- src/raycaster.cpp
+++ src/raycaster.cpp
@@ -155,89 +155,11 @@ Vec3 Scene::pointLightColor(const Vec3& point, const Vec3& normal, const Materia
     }
     return light_attenuation * point_light.color.multiply(material.diffuse);
 }
-Image::Image(int x_resolution, int y_resolution)
-    : x_resolution(x_resolution), y_resolution(y_resolution) {
-  assert(x_resolution > 0);
-  assert(y_resolution > 0);
-  buffer.resize((size_t)x_resolution * (size_t)y_resolution);
-}
-void Image::scaleMaxTo(double new_max) {
-  assert(new_max > EPS);
-  double old_max = maxColorValue();
-  if (old_max < EPS) {
-    return;
-  }
-  double scale_factor = new_max / old_max;
-  for (int y = 0; y < y_resolution; ++y) {
-    for (int x = 0; x < x_resolution; ++x) {
-      size_t pos = (size_t)bufferPos(x, y);
-      buffer[pos] = buffer[pos] * scale_factor;
-    }
   }
 }
-bool Image::savePPM(const std::string& file) {
-  std::ofstream fout(file, std::ios::binary);
-  if (!fout.is_open()) {
-    return false;
   }
-  // binary mode
-  fout << "P6" << std::endl;
-  fout << x_resolution << " " << y_resolution << std::endl;
-  fout << "255" << std::endl;
-  for (int y = 0; y < y_resolution; ++y) {
-    for (int x = 0; x < x_resolution; ++x) {
-      // r g b
-      for (int i = 0; i < 3; ++i) {
-        fout << floatToByte(linearToSrgb(buffer[(size_t)bufferPos(x, y)][i]));
-      }
-    }
   }
-  fout.close();
-  return true;
-}
-// Converts linear intensities to sRGB values
-// ("gamma" encoding)
-// Human eye perceives light in a logarithmic scale.
-// Therefore encoding the light intensities without
-// any transformation would leave little (perceived)
-// resolution to dim intensities.
-//
-// Note that this is not a logarithmic tranformation but
-// apparently still works well.
-//
-// see http://color.org/chardata/rgb/sRGB.pdf
-double Image::linearToSrgb(double val) {
-  if (val <= 0.0031308) {
-    return 12.92 * val;
-  }
-  return 1.055 * std::pow(val, 1.0 / 2.4) - 0.055;
-}
-double Image::maxColorValue() {
-  double max_value = 0;
-  for (int y = 0; y < y_resolution; ++y) {
-    for (int x = 0; x < x_resolution; ++x) {
-      for (int i = 0; i < 3; ++i) {
-        max_value = std::max(max_value, buffer[(size_t)bufferPos(x, y)][i]);
-      }
-    }
   }
   return max_value;
 }
-Image generateGammaTestImage(int x_size, int y_size) {
-  assert(x_size > 20 && y_size > 20);
-  Image image(x_size, y_size);
-  for (int y = 0; y < y_size; ++y) {
-    for (int x = 0; x < x_size / 2; ++x) {
-      Vec3 color(0.0);
-      if ((x + y) % 2 == 0) {
-        color = Vec3(1.0);
-      }
-      image.setColor(x, y, color);
-    }
-    for (int x = x_size / 2; x < x_size; ++x) {
-      Vec3 color(0.5);
-      image.setColor(x, y, color);
-    }
-  }
-  return image;
 }
