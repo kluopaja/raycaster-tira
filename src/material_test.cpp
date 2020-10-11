3063ef3b64fd6a8cb2f1d626c86e6eb540ca7808
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2020-10-11 14:40:26 +0300

    Improve Material class tests
      Modify
        TEST(RenderTest, OpaqueDiffuseImportanceSampling)
        TEST(Material, ApplyOpaqueDiffuseOnObliqueLight)
        TEST(Material, ApplyTransparentDiffuseOnObliqueLight)
      so that they will detect if different channels are being
      handled differently. Also having Material.diffuse values
      different from 1.0 will allow more problems to be detected.

--- src/material_test.cpp
+++ src/material_test.cpp
@@ -17,7 +17,7 @@ MATCHER_P(VecEq, v, "should equal " + PrintToString(v)) {
 // light is shone directly on it
 TEST(Material, ApplyOpaqueDiffuseOnPerpendicularLight) {
   Material m = Material();
-  m.diffuse = Vec3(1.0, 1.0, 1.0);
+  m.diffuse = Vec3(0.1, 0.2, 0.3);
   Vec3 normal(0.0, 1.0, 0.0);
   std::mt19937 mt(1337);
   for(int i = 0; i < 100; ++i) {
@@ -26,12 +26,12 @@ TEST(Material, ApplyOpaqueDiffuseOnPerpendicularLight) {
     in_vector /= in_vector.norm();
     Vec3 in_color(1.0, 1.0, 1.0);
     Vec3 out_color = m.apply(in_vector, normal, out_vector, in_color);
-    ASSERT_THAT(out_color, VecEq(Vec3(1.0) / kPi));
+    ASSERT_THAT(out_color, VecEq(Vec3(0.1, 0.2, 0.3) / kPi));
   }
 }
 TEST(Material, ApplyOpaqueDiffuseOnObliqueLight) {
   Material m = Material();
-  m.diffuse = Vec3(1.0, 1.0, 1.0);
+  m.diffuse = Vec3(0.1, 0.2, 0.3);
   Vec3 normal(0.0, 1.0, 0.0);
   std::mt19937 mt(1337);
   for(int i = 0; i < 100; ++i) {
@@ -39,13 +39,13 @@ TEST(Material, ApplyOpaqueDiffuseOnObliqueLight) {
     Vec3 in_vector = uniformRandomHemispherePoint(normal, mt);
     Vec3 in_color(1.0, 1.0, 1.0);
     Vec3 out_color = m.apply(in_vector, normal, out_vector, in_color);
-    Vec3 correct = Vec3(1.0) / kPi * normal.dot(in_vector);
+    Vec3 correct = Vec3(0.1, 0.2, 0.3) / kPi * normal.dot(in_vector);
     ASSERT_THAT(out_color, VecEq(correct));
   }
 }
 TEST(Material, ApplyTransparentDiffuseOnObliqueLight) {
   Material m = Material();
-  m.diffuse = Vec3(1.0, 1.0, 1.0);
+  m.diffuse = Vec3(0.1, 0.2, 0.3);
   m.transparent = Vec3(1.0);
   Vec3 normal(0.0, 1.0, 0.0);
   std::mt19937 mt(1337);
@@ -54,7 +54,7 @@ TEST(Material, ApplyTransparentDiffuseOnObliqueLight) {
     Vec3 in_vector = uniformRandomHemispherePoint(normal, mt);
     Vec3 in_color(1.0, 1.0, 1.0);
     Vec3 out_color = m.apply(in_vector, normal, out_vector, in_color);
-    Vec3 correct = Vec3(1.0) / (2 * kPi) * normal.dot(in_vector);
+    Vec3 correct = Vec3(0.1, 0.2, 0.3) / (2 * kPi) * normal.dot(in_vector);
     ASSERT_THAT(out_color, VecEq(correct));
   }
 }
